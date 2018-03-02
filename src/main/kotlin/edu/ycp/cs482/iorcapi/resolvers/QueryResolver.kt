0421d91610a6f2b22a3ea53b8bc58daa3a7664d2
Aaron <ledzeppelindrummer@gmail.com>
2018-03-02 16:04:43 -0500

    Added run profiles so that we can switch between debug and production. Spun up some free-tier AWS-based mongodb servers in a cluster.

--- src/main/kotlin/edu/ycp/cs482/iorcapi/resolvers/QueryResolver.kt
+++ src/main/kotlin/edu/ycp/cs482/iorcapi/resolvers/QueryResolver.kt
@@ -1,9 +1,15 @@
 package edu.ycp.cs482.iorcapi.resolvers
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
 @Component
 class QueryResolver : GraphQLQueryResolver {
-    fun version() = "0.0.1d"
+
+    @Value("\${spring.application.name}")
+    private val appName: String = ""
+
+    fun version() = appName
+
 }
