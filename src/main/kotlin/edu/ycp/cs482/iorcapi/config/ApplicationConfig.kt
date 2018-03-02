df6be29cf444d7b17bcfb2e7cdd68ca9b7e8ff4e
Aaron <ledzeppelindrummer@gmail.com>
2018-03-02 16:04:43 -0500

    Added run profiles so that we can switch between debug and production. Spun up some free-tier AWS-based mongodb servers in a cluster.

--- src/main/kotlin/edu/ycp/cs482/iorcapi/config/ApplicationConfig.kt
+++ src/main/kotlin/edu/ycp/cs482/iorcapi/config/ApplicationConfig.kt
@@ -2,6 +2,8 @@ package edu.ycp.cs482.iorcapi.config
 
 import com.mongodb.Mongo
 import com.mongodb.MongoClient
+import com.mongodb.MongoClientURI
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration
@@ -11,12 +13,23 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @Configuration
 @EnableMongoRepositories(basePackages = ["edu.ycp.cs482.iorcapi.repositories"])
 open class ApplicationConfig : AbstractMongoConfiguration(){
+
+    @Value("\${spring.profiles.active}")
+     private val profileActive: String? = null
+
+//    @Value("\${spring.application.name}")
+//    private val proAppName: String? = null
+
+    @Value("\${spring.data.mongodb.uri}")
+    private val mongoUri: String? = null
+
+
     override fun getDatabaseName(): String {
         return "iOrcDb"
     }
 
     override fun mongo(): Mongo {
-        return MongoClient("localhost")
+        return MongoClient(MongoClientURI(mongoUri))
     }
 
     @Bean
