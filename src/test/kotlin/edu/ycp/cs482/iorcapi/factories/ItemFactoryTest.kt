c9182ab3063aa3c8f02743968773ca333fe0ecb6
Hunter WIllingham <hwilling@ycp.edu>
2018-03-11 01:12:53 -0500

    basic implementation of get version item type

--- src/test/kotlin/edu/ycp/cs482/iorcapi/factories/ItemFactoryTest.kt
+++ src/test/kotlin/edu/ycp/cs482/iorcapi/factories/ItemFactoryTest.kt
@@ -61,7 +61,7 @@ class ItemFactoryTest {
         )
 
         //get by version the list of items
-        val itemFactList = itemFactory.getVersionItemType("TEST_VERSION", type = ObjType.ITEM_WEAPON)
+        val itemFactList = itemFactory.getVersionItems("TEST_VERSION")
         //print("FACTORY ITEMS" + itemFactList)
         val repoItems = itemRepository.findByVersion("TEST_VERSION")
         //print("REPO ITEMS" + repoItems)
@@ -73,12 +73,52 @@ class ItemFactoryTest {
 
     @Test
     fun getVersionItemType() {
-        //TODO implement
+        //TODO improve and expand
+        //itemRepository
+        itemFactory.addItem(
+                name = "Battle Axe of the Not so Bold",
+                description = "A battle axe that is wielded people who want a useless item worth way too much money.",
+                price = 999999f,
+                itemClasses = listOf("axe", "military_weapon", "melee_weapon"),
+                version = "TEST_VERSION",
+                type = ObjType.ITEM_WEAPON
+        )
+
+        //get by version and item type the list of items
+        val itemFactList = itemFactory.getVersionItemType("TEST_VERSION", type = ObjType.ITEM_WEAPON)
+        //print("FACTORY ITEMS" + itemFactList)
+        val repoItems = itemRepository.findByVersion("TEST_VERSION")
+        //print("REPO ITEMS" + repoItems)
+
+        //test the list of items
+        assertThat(repoItems.get(0).id, `is` (equalTo(itemFactList.get(0).id)))
+        assertThat(repoItems.get(0).name, `is` (equalTo(itemFactList.get(0).name)))
+
     }
 
     @Test
     fun getItemsByClasses() {
-        //TODO implement
+        //TODO improve and expand, add more items with different classes and add them to list
+        //itemRepository
+        /*val test = itemFactory.addItem(
+                name = "Battle Axe of the Not so Bold",
+                description = "A battle axe that is wielded people who want a useless item worth way too much money.",
+                price = 999999f,
+                itemClasses = listOf("axe", "military_weapon", "melee_weapon"),
+                version = "TEST_VERSION",
+                type = ObjType.ITEM_WEAPON
+        )
+        val repoItem = itemRepository.findById(test.id)
+        print("TEST ITEM" + repoItem?.name)
+        //get by version and item type the list of items
+        val itemFactList = itemFactory.getItemsByClasses("TEST_VERSION", classes = listOf("military_weapon"))
+        print("FACTORY ITEMS" + itemFactList)
+        val repoItems = itemRepository.findByVersionAndItemClasses("TEST_VERSION", classes = listOf("military_weapon"))
+        print("REPO ITEMS" + repoItems)
+
+        //test the list of items
+        assertThat(repoItems.get(0).id, `is` (equalTo(itemFactList.get(0).id)))
+        assertThat(repoItems.get(0).name, `is` (equalTo(itemFactList.get(0).name)))*/
     }
 
     @Test
